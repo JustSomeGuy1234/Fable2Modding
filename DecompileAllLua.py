cd2d4da1494bea7132486b91f274c2c0910162d5
JustSomeGuy1234 <d.southren-dale@hotmail.com>
2022-04-20 22:40:13 +0100

    Added DecompileAllLua.py

---
+++ DecompileAllLua.py
@@ -0,0 +1,72 @@
+import sys
+import os
+import subprocess
+import io
+import glob
+
+
+passedFolder = sys.argv[1]
+
+allLuas = glob.glob(passedFolder + "/**/*.lua", recursive=True)
+
+for thisFile in allLuas:
+	print("In: " + thisFile)
+	
+	# Determine the bnk the scripts are from. Now that I think about it, I don't think this is actually necessary as sys.argv[1] starts at the /bnk script's/ path, and not the drive letter.
+	bnkName = ""
+	gs = "\\gamescripts\\scripts\\"
+	gsr = "\\gamescripts_r\\scripts\\"
+	guis = "\\guiscripts\\scripts\\"
+	guisa = "\\guiscripts\\art\\"
+
+	hasFoundBankName = False
+	if thisFile.find(gs) > -1:
+		bnkName = gs
+		hasFoundBankName = True
+	if thisFile.find(gsr) > -1:
+		bnkName = gsr
+		hasFoundBankName = True
+	if thisFile.find(guis) > -1:
+		bnkName = guis
+		hasFoundBankName = True
+	if thisFile.find(guisa) > -1:
+		bnkName = guisa
+		hasFoundBankName = True
+
+	# Get the file name, file folder, output folder
+	thisFileName = thisFile[thisFile.rfind("\\"):len(thisFile)]
+	thisFileFolder = thisFile[0:thisFile.rfind("\\")]
+	if not hasFoundBankName or thisFileFolder.find(bnkName) == -1 or bnkName == "":
+		print("Couldn't find: " + bnkName + "\nin file path!")
+		print("hasFoundBankName is " + str(hasFoundBankName))
+		print("filefolder is: " + thisFileFolder)
+		input()
+		break
+	outFolder = thisFileFolder.replace(bnkName, bnkName + "\\decompiled\\")
+	outFile = outFolder+thisFileName
+	print("Out: " + outFile)
+
+	# Make output folder if it doesn't exist
+	if not os.path.exists(outFolder):
+		try:
+			os.makedirs(outFolder)
+		except OSError as err:
+			print("Msg: Failed to create output folder.")
+			print(err)
+
+	# Make sure I didn't do something stupid and try and overwrite the original file
+	if outFile == thisFile:
+		print("Output path is the same as input path! Wut")
+		input()
+		break
+
+	# Finale
+	unluacArgs = "Java -jar ./unluac.jar " + '"' + thisFile + '"' + " >> " + '"' + outFile + '"'
+	print("CMD ARGS: " + unluacArgs)
+	result = os.system(unluacArgs)
+	if result == 1:
+		print("unluac error. Probably because the script is already plaintext. Copying file.")
+		os.remove(outFile)
+		os.system("copy " + thisFile + " " + outFile)
+
+input("\n\nFinished")
